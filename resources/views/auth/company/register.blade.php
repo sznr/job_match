9fed18999025906b1bbaa31e3901de5026731d6d
narusesuzuki <satoshisennsyu@gmail.com>
2021-10-23 06:46:02 +0900

    ミス修正

--- resources/views/auth/company/register.blade.php
+++ resources/views/auth/company/register.blade.php
@@ -45,7 +45,7 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('company.login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
--- resources/views/auth/user/register.blade.php
+++ resources/views/auth/user/register.blade.php
@@ -46,7 +46,7 @@
             @endif
 
             <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
+                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('user.login') }}">
                     {{ __('Already registered?') }}
                 </a>
 
