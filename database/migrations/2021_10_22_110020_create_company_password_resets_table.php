4e5dde4ef78286ff6d90848e60585da0c1f4ba58
narusesuzuki <satoshisennsyu@gmail.com>
2021-10-22 11:01:06 +0900

    fix

--- database/migrations/2021_10_22_110020_create_company_password_resets_table.php
+++ database/migrations/2021_10_22_110020_create_company_password_resets_table.php
@@ -14,8 +14,9 @@ class CreateCompanyPasswordResetsTable extends Migration
     public function up()
     {
         Schema::create('company_password_resets', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
